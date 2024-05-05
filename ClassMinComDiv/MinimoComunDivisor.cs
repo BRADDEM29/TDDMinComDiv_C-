85bc1d29d8bfb527836db86b64a91b48a6e026a6
BRADDEM29 <72587556@continental.edu.pe>
2024-05-04 22:26:03 -0500

    Agregar archivos de proyecto.

---
+++ ClassMinComDiv/MinimoComunDivisor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMinComDiv
+{
+    public class MinimoComunDivisor
+    {
+        public static int MinComDivi(int nro1, int nro2, int nro3, int nro4)  
+        {
+            int mcd_abcd = MCD(MCD(nro1, nro2), MCD(nro3, nro4));
+
+            return mcd_abcd;
+
+        }
+        public static int MCD(int a, int b)
+        {
+            // Algoritmo de Euclides para encontrar el MCD
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+
+        }
+
+    }
+}
